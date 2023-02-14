a4a74790d4996c1048b58e5a1e0c9298cb3f84da
Sofuto22 <zenigata19@gmail.com>
2023-02-14 15:32:04 -0500

    started thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,97 +1,19 @@
-const { User } = require("../models");
+const { Thought, User } = require("../models");
+const { getAllUsers } = require("./user-controller");
 
-const userController = {
+const thoughtController = { 
 
-    getAllUsers(req,res) {
-        User.find({})
-        .populate({path: "thoughts", select:"-__v"})
-        .populate({path: "friends", select: "-__v"})
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({path: "reactions", select: "-__v"})
+        .select("-__v")
         .sort({_id: -1})
         .then((dbUserData) => res.json(dbUserData))
-        console.log(error);
-        res.status(500).json(error);
-    },
-};
+        .catch((err) => {
+            res.status(400).json(err)
+           });
+        },
 
-    getUserById({params}, res) {
-        User.findOne({_id: params.userId})
-        .select("-__v")
-        .populate ({ path:friends,})
-        .populate (thoughts,)
+     getThoughtById({params})   
 
-        .then(dbUserData => {
-            if(!dbUserData) {
-                res.status(404).json({message:"No user found!"});
-                return;
-            }
-
-        })
-
-    };
-
-    createUser({body}, res) {
-        User.create(body)
-        .then(dbUserData => res.json(dbUserData))
-        .catch(error => res.status(400).json(error));
-    };
-
-    udpateUser({body}, res) {
-        User.findOneAndUpdate({id:params.userId}, body, {
-            new:true,
-
-        })
-
-        .then(dbUserData =>
-            {
-                if(!dbUserData) {
-                    res.status(404).json({message:"No user found!"})
-                    return;
-                }
-            })
-    };
-
-    removeUser({body}, res) {
-        userController.findOneAndDelete({id:params.userId}, body, {
-            new:true,
-        })
-
-        .then(dbUserData => 
-            {
-                if(!dbUserData) {
-                    res.status(404).json({message:"No user found!"})
-                }
-            })
-    };
-
-    addFriend ({params}, res) {
-        User.findOneAndUpdate({_id: params}, {path: { friends: params.friendID}},
-        {new:true})
-        .select("-__v")
-        .populate ({ path:friends,})
-        .then(dbUserData =>
-            {
-                if(!dbUserData) {
-                    res.status(404).json({message:"No user found!"});
-                    return;
-                }
-                })
-            
-
-    };
-
-    deleteFriend ({params}, res) {
-        User.findOneAndDelete({_id: params}, {path: { friends: params.friendID}},
-        {new:true})
-        .select("-__v")
-        .populate ({ path:friends,})
-        .then(dbUserData =>
-            {
-                if(!dbUserData) {
-                    res.status(404).json({message:"No user found!"});
-                    return;
-                }
-                })
-            };
-
-
-            module.exports = userController;
+}
