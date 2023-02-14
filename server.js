673bd133477745094cc91cef7c447dcb130e431a
Sofuto22 <zenigata19@gmail.com>
2023-02-14 13:15:02 -0500

    moved folders to correct spot

---
+++ server.js
@@ -0,0 +1,24 @@
+const { debug } = require("console");
+const express = require ("express");
+const mongoose = require ("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use (express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect (
+    process.env.MONGODB_URI || "mongodb://localhost/social-network",
+{
+    useFindAndModify: false,
+    useUnifiedTopolgy: true,
+    useNewUrlParser: true,
+
+}
+
+);
+
+mongoose.set("debug", true);
+
+app.list(PORT, () => console.log("Connected on localhost:${PORT}"));
