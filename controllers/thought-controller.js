2d766dd2cc006fde9138636cc5c95d62d6faf1b2
Sofuto22 <zenigata19@gmail.com>
2023-02-14 13:36:18 -0500

    started controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,36 @@
+const { User } = require("../models");
+
+const userController = {
+
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({path: "thoughts", select:"-__v"})
+        .populate({path: "friends", select: "-__v"})
+        .sort({_id: -1})
+        .then((dbUserData) => res.json(dbUserData))
+        console.log(error);
+        res.status(500).json(error);
+    },
+};
+
+    getUserById({params}, res) {
+        User.findOne({_id: params.userId})
+        .select("-__v")
+        .populate ({ path:friends,})
+        .populate (thoughts,)
+
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message:"No user found!"});
+                return;
+            }
+
+        })
+
+    },
+
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(error => res.status(400).json(error));
+    },
